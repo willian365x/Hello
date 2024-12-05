635bde3d54aa7393df3bf02e4a8f9dc66f710763
willian365x <willian365x@gmail.com>
2024-12-05 01:37:30 -0300

    modified: Expression

---
+++ src/main/kotlin/ExpressaoIF.kt
@@ -0,0 +1,6 @@
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun main() {
+    println(max(-1, 5))
+    println(max(-1, -5))
+}
---
+++ src/main/kotlin/VerificacaoIgualdade.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val frutas = setOf("maçã","pêra","uva")
+    //val fruits = frutas
+    val fruits = setOf("uva","maçã","pêra")
+
+    println(frutas == fruits)
+    println(frutas === fruits)
+}
