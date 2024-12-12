6c40cffbf6164af00148fc2cf5fa9e900e1f5991
willian365x <willian365x@gmail.com>
2024-12-12 16:13:17 -0300

    modified: ListaMap

---
+++ src/main/kotlin/ListaMap.kt
@@ -0,0 +1,22 @@
+val x_point = 15
+val ezListMapPoints: MutableMap<Int,Int> = mutableMapOf(1 to 100, 2 to 100, 3 to 100)
+
+fun ezReport(){
+    ezListMapPoints.forEach { (t, u) -> println("Id: $t = $u points!") }
+}
+
+fun updatePoints(accountId: Int){
+    if(ezListMapPoints.containsKey(accountId)){
+        ezListMapPoints[accountId] = ezListMapPoints.getValue(accountId) + x_point
+    }else{
+        println("Error! Id: $accountId not exists!")
+    }
+}
+
+fun main() {
+    ezReport()
+    updatePoints(1)
+    updatePoints(1)
+    updatePoints(5)
+    ezReport()
+}
